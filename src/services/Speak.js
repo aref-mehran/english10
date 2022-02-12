45a40921735a53093e7dc525b637e23d16201b83
Your Name <you@example.com>
2022-02-12 20:36:26 +0330

    word and sentence mode added.

--- src/services/Speak.js
+++ src/services/Speak.js
@@ -1,24 +1,25 @@
 import Speech from 'speak-tts'
 class Speak {
     speech = null;
-    constructor() {
+    constructor( ) {
         this.speech = new Speech() // will throw an exception if not browser supported
         if (this.speech.hasBrowserSupport()) { // returns a boolean
             console.log("speech synthesis supported")
-            const firstOfflineVoice = speechSynthesis.getVoices().filter((el) => {
-                return el.localService == true;
-            })[0]?.name;
+            
 
-            this.speech.init({
-                volume: 1,
-                lang: "en-GB",
-                rate: 1,
-                pitch: 1,
-                voice: firstOfflineVoice
-            })
         }
     }
-    read(text) {
+    read(text , speed) {
+        const firstOfflineVoice = speechSynthesis.getVoices().filter((el) => {
+            return el.localService == true;
+        })[0]?.name;
+        this.speech.init({
+            volume: 1,
+            lang: "en-GB",
+            rate: speed,
+            pitch: 1,
+            voice: firstOfflineVoice
+        })
         this.speech.speak({
                 text: text,
                 queue: false
--- src/views/Lesson.vue
+++ src/views/Lesson.vue
@@ -1,8 +1,38 @@
 <template>
-    <div  @click="clickOnSentence" >
+    <div  @click="clicked" >
         <p >
-            this is Losson : {{this.lesson}}
+            this is Lesson : {{this.lesson}}
         </p>
+        <v-container class="grey lighten-5" :align="alignments">
+        <!--<h1> this is Lossone : {{this.lesson}} </h1>-->
+            <v-row no-gutters :align="alignments">
+                <v-col
+                    cols="12"
+                    sm="4"
+                >
+                    <v-slider
+                        v-model="sliderValue"
+                        step="5"
+                        thumb-label
+                        ticks
+                        :max="200"
+                    ></v-slider> 
+                </v-col>    
+                <v-col
+                    cols="12"
+                    sm="4"
+                >                   
+                    <v-autocomplete
+                        v-model="autocompleteValue"
+                        :items="autocompleteItems"
+                        dense
+                        filled
+                        label="choose model"
+                    >
+                    </v-autocomplete>                
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
@@ -12,7 +42,12 @@ import Speak from '@/services/Speak.js'
 export default {
     data(){
         return{
-            lesson :''
+            lesson :'',
+            alignments: 'center',
+            sliderValue: 100 ,
+            autocompleteItems: ['Word', 'Sentence'],
+            autocompleteValue: 'Sentence',
+            //pdfPath: require('@/assets/pdfs/lessonone.pdf'),
         }
     },
     mounted(){
@@ -24,16 +59,32 @@ export default {
         //speak.read("This is text to speech test.")
     },
     methods:{
-    clickOnSentence() {
-        let cursor = window.getSelection();
+        clickOnSentence() {
+            let cursor = window.getSelection();
 
-        cursor.modify("move", "backward", "lineboundary");
-        cursor.modify("extend", "forward", "lineboundary");
-        var selectedStr = cursor.toString();
+            cursor.modify("move", "backward", "lineboundary");
+            cursor.modify("extend", "forward", "lineboundary");
+            var selectedStr = cursor.toString();
 
-        selectedStr = selectedStr.trim();
-        this.speak.read(selectedStr);
-    }
+            selectedStr = selectedStr.trim();
+            this.speak.read(selectedStr , this.sliderValue/100);
+        },
+        clickOnWord() {
+            let cursor = window.getSelection();
+            cursor.modify("move", "backward", "word");
+            cursor.modify("extend", "forward", "word");
+            var selectedStr = cursor.toString();
+
+            this.speak.read(selectedStr , this.sliderValue/100);
+            //setSelected(res);
+        },
+        clicked (){
+            if (this.autocompleteValue === 'Sentence') {
+                this.clickOnSentence();
+            } else {
+                this.clickOnWord();
+            }
+        }
     }
 }
 </script>
