924f7cc4d419567500897f1f234c16ea51a33e9c
Your Name <you@example.com>
2022-02-13 22:02:49 +0330

    unused file deleted.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,13 +37,12 @@
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+      
     },
     methods:{
         lessonItemClick(itemTitle){
