076372d351d3a5dbab630c2cd90756229c2ad30b
Your Name <you@example.com>
2022-02-13 22:56:47 +0330

    data read from lessonItem.js

---
+++ src/data/lessonItems.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        avatar: require('@/assets/image/2.jpg'),
+        title: 'Lesson one',
+        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+    },
+    { divider: true, inset: true },
+    {
+        avatar: require('@/assets/image/2.jpg'),
+        title: 'Lesson two',
+        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+    },
+    { divider: true, inset: true },
+    {
+        avatar: require('@/assets/image/2.jpg'),
+        title: 'Lesson Three',
+        subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+    },
+    { divider: true, inset: true },
+    {
+        avatar: require('@/assets/image/2.jpg'),
+        title: 'Lesson Four',
+        subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+    },
+    { divider: true, inset: true },
+    {
+        avatar: require('@/assets/image/2.jpg'),
+        title: 'Lesson Five',
+        subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+    }
+]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     class="mx-auto"
   >
     <v-list three-line>
-      <template v-for="(item, index) in items">
+      <template v-for="(item, index) in items ">
         <v-subheader
           v-if="item.header"
           :key="item.header"
@@ -37,10 +37,9 @@
 </template>
 
 <script>
-
+import lessonItems from '@/data/lessonItems.js'
   export default {
     name: 'Home',
-
     components: {
       
     },
@@ -52,38 +51,7 @@
     },
     data:()=>({
         selectedLesson :'',
-        items: [
-        { header: 'English for schools' },
-        {
-          avatar: require('@/assets/image/2.jpg'),
-          title: 'Lesson one',
-          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: require('@/assets/image/2.jpg'),
-          title: 'Lesson two',
-          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: require('@/assets/image/2.jpg'),
-          title: 'Lesson Three',
-          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: require('@/assets/image/2.jpg'),
-          title: 'Lesson Four',
-          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: require('@/assets/image/2.jpg'),
-          title: 'Lesson Five',
-          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-        },
-      ],
+        items : lessonItems
     })
   }
 </script>
