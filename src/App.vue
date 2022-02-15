e514e94902e5a12963819a640f9d948e48a7f6e7
Your Name <you@example.com>
2022-02-15 09:21:34 +0330

    footer file created

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <v-app>
-    <Header 
-      :correctCount="correctCount"
-      :totalCount="totalCount"
-     />
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <Header 
+            :correctCount="correctCount"
+            :totalCount="totalCount"
+        />
+        <v-main>
+            <router-view/>
+        </v-main>
+        <Footer />
+    </v-app>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
     
   name: 'App',
     components: {
     Header,
+    Footer
   },
   data: () => ({
     //
