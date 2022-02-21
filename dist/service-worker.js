4007e908428431fccf9950b0ae380e9eaab89f1d
Your Name <you@example.com>
2022-02-21 21:01:52 +0330

    dist folder updated

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/english10/dist/precache-manifest.5cf0db59023b25b494ffad0e8b515051.js"
+  "/english10/dist/precache-manifest.21d22d7cda015e123f8ff894feb9e848.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "english10"});
