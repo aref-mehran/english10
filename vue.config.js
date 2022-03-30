7bd0d484866bfc15b050067e95cdfc4c2c350c5b
Your Name <you@example.com>
2022-03-30 08:51:04 +0000

    publicPath changed to  /

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
     
       },
      publicPath: process.env.NODE_ENV === 'production'
-    ? '/english10/dist/'
+    ? '/'
     : '/',
 	transpileDependencies: [
     'vuetify'
