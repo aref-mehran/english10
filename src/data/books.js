e9e874c2e2be846d913f91208d69ad031c87070a
John Doe <johndoe@example.com>
2022-04-08 16:05:33 +0430

    home menu changed to have other books.

---
+++ src/data/books.js
@@ -0,0 +1,162 @@
+export default [
+  {
+    bookName: "انگلیسی دهم",
+    avatar: require("@/assets/image/lesson1.png"),
+    lessons: [
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson one",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 15, endPageNum: 18 },
+          { title: "Conversation", startPageNum: 19, endPageNum: 19 },
+          { title: "New Words & Expressions", startPageNum: 20, endPageNum: 21 },
+          { title: "Reading", startPageNum: 22, endPageNum: 23 },
+          { title: "Grammar", startPageNum: 24, endPageNum: 29 },
+          { title: "Listening & Speaking", startPageNum: 30, endPageNum: 31 },
+          { title: "Pronunciation", startPageNum: 32, endPageNum: 33 },
+          { title: "Writing", startPageNum: 34, endPageNum: 39 },
+          { title: "What You Learned", startPageNum: 40, endPageNum: 41 },
+        ],
+        subtitle: `<span class="text--primary">Saving Nature (15-41) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/lesson2.png"),
+        title: "Lesson two",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 43, endPageNum: 46 },
+          { title: "Conversation", startPageNum: 47, endPageNum: 47 },
+          { title: "New Words & Expressions", startPageNum: 48, endPageNum: 49 },
+          { title: "Reading", startPageNum: 50, endPageNum: 51 },
+          { title: "Grammar", startPageNum: 52, endPageNum: 57 },
+          { title: "Listening & Speaking", startPageNum: 58, endPageNum: 59 },
+          { title: "Pronunciation", startPageNum: 60, endPageNum: 61 },
+          { title: "Writing", startPageNum: 62, endPageNum: 67 },
+          { title: "What You Learned", startPageNum: 68, endPageNum: 69 },
+        ],
+        subtitle: `<span class="text--primary"> Wonders of Creation (43-69) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/lesson3.png"),
+        title: "Lesson Three",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 71, endPageNum: 75 },
+          { title: "Conversation", startPageNum: 76, endPageNum: 76 },
+          { title: "New Words & Expressions", startPageNum: 77, endPageNum: 79 },
+          { title: "Reading", startPageNum: 80, endPageNum: 81 },
+          { title: "Grammar", startPageNum: 82, endPageNum: 86 },
+          { title: "Listening & Speaking", startPageNum: 87, endPageNum: 88 },
+          { title: "Pronunciation", startPageNum: 89, endPageNum: 90 },
+          { title: "Writing", startPageNum: 91, endPageNum: 93 },
+          { title: "What You Learned", startPageNum: 94, endPageNum: 95 },
+        ],
+        subtitle: `<span class="text--primary">TheValue of Knowledge (71-95) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/lesson4.png"),
+        title: "Lesson Four",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 97, endPageNum: 101 },
+          { title: "Conversation", startPageNum: 102, endPageNum: 102 },
+          { title: "New Words & Expressions", startPageNum: 103, endPageNum: 104 },
+          { title: "Reading", startPageNum: 105, endPageNum: 106 },
+          { title: "Grammar", startPageNum: 107, endPageNum: 111 },
+          { title: "Listening & Speaking", startPageNum: 112, endPageNum: 113 },
+          { title: "Pronunciation", startPageNum: 114, endPageNum: 114 },
+          { title: "Writing", startPageNum: 115, endPageNum: 117 },
+          { title: "What You Learned", startPageNum: 118, endPageNum: 119 },
+        ],
+        subtitle: `<span class="text--primary">TheValue of Knowledge (97-119) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/appendix.png"),
+        title: "Appendix",
+        sectionItems: [
+          { title: "Irregular Verbs", startPageNum: 121, endPageNum: 125 },
+        ],
+        subtitle: `<span class="text--primary">Irregular Verbs (121-125) </span> `,
+      },
+
+    ],
+  },
+  {
+    bookName: "انگلیسی یازدهم",
+    avatar: require("@/assets/image/lesson1.png"),
+    lessons: [
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson one",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 15, endPageNum: 18 },
+          { title: "Conversation", startPageNum: 19, endPageNum: 19 },
+          { title: "New Words & Expressions", startPageNum: 20, endPageNum: 21 },
+          { title: "Reading", startPageNum: 22, endPageNum: 23 },
+          { title: "Grammar", startPageNum: 24, endPageNum: 29 },
+          { title: "Listening & Speaking", startPageNum: 30, endPageNum: 31 },
+          { title: "Pronunciation", startPageNum: 32, endPageNum: 33 },
+          { title: "Writing", startPageNum: 34, endPageNum: 39 },
+          { title: "What You Learned", startPageNum: 40, endPageNum: 41 },
+        ],
+        subtitle: `<span class="text--primary">Saving Nature (15-41) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/lesson2.png"),
+        title: "Lesson two",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 43, endPageNum: 46 },
+          { title: "Conversation", startPageNum: 47, endPageNum: 47 },
+          { title: "New Words & Expressions", startPageNum: 48, endPageNum: 49 },
+          { title: "Reading", startPageNum: 50, endPageNum: 51 },
+          { title: "Grammar", startPageNum: 52, endPageNum: 57 },
+          { title: "Listening & Speaking", startPageNum: 58, endPageNum: 59 },
+          { title: "Pronunciation", startPageNum: 60, endPageNum: 61 },
+          { title: "Writing", startPageNum: 62, endPageNum: 67 },
+          { title: "What You Learned", startPageNum: 68, endPageNum: 69 },
+        ],
+        subtitle: `<span class="text--primary"> Wonders of Creation (43-69) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/lesson3.png"),
+        title: "Lesson Three",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 71, endPageNum: 75 },
+          { title: "Conversation", startPageNum: 76, endPageNum: 76 },
+          { title: "New Words & Expressions", startPageNum: 77, endPageNum: 79 },
+          { title: "Reading", startPageNum: 80, endPageNum: 81 },
+          { title: "Grammar", startPageNum: 82, endPageNum: 86 },
+          { title: "Listening & Speaking", startPageNum: 87, endPageNum: 88 },
+          { title: "Pronunciation", startPageNum: 89, endPageNum: 90 },
+          { title: "Writing", startPageNum: 91, endPageNum: 93 },
+          { title: "What You Learned", startPageNum: 94, endPageNum: 95 },
+        ],
+        subtitle: `<span class="text--primary">TheValue of Knowledge (71-95) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/lesson4.png"),
+        title: "Lesson Four",
+        sectionItems: [
+          { title: "get Ready", startPageNum: 97, endPageNum: 101 },
+          { title: "Conversation", startPageNum: 102, endPageNum: 102 },
+          { title: "New Words & Expressions", startPageNum: 103, endPageNum: 104 },
+          { title: "Reading", startPageNum: 105, endPageNum: 106 },
+          { title: "Grammar", startPageNum: 107, endPageNum: 111 },
+          { title: "Listening & Speaking", startPageNum: 112, endPageNum: 113 },
+          { title: "Pronunciation", startPageNum: 114, endPageNum: 114 },
+          { title: "Writing", startPageNum: 115, endPageNum: 117 },
+          { title: "What You Learned", startPageNum: 118, endPageNum: 119 },
+        ],
+        subtitle: `<span class="text--primary">TheValue of Knowledge (97-119) </span> `,
+      },
+      {
+        avatar: require("@/assets/image/appendix.png"),
+        title: "Appendix",
+        sectionItems: [
+          { title: "Irregular Verbs", startPageNum: 121, endPageNum: 125 },
+        ],
+        subtitle: `<span class="text--primary">Irregular Verbs (121-125) </span> `,
+      },
+
+    ],
+  }
+
+
+];
