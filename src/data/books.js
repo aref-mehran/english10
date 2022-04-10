3740fb1b60b9ade178de120d0f7289ca84939ab3
Your Name <you@example.com>
2022-04-10 12:05:28 +0430

    book 7 , 8 , 9 added to books.js

--- src/data/books.js
+++ src/data/books.js
@@ -1,5 +1,595 @@
 export default [
   {
+    bookTitle: "انگلیسی هفتم",
+    bookName: "english7",
+    pageOffset: 12,
+    avatar: require("@/assets/image/lesson1.png"),
+    lessons: [
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson one",
+        subtitle: `<span class="text--primary">My Name</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 6, endPageNum: 7 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 8,
+            endPageNum: 8,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 9,
+            endPageNum: 9,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 9,
+            endPageNum: 9,
+          },
+          {
+            title: "Your Conversation",
+            startPageNum: 9,
+            endPageNum: 9,
+          },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Two",
+        subtitle: `<span class="text--primary">My Classmates</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 10, endPageNum: 11 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 12,
+            endPageNum: 12,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 13,
+            endPageNum: 13,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 13,
+            endPageNum: 13,
+          },
+          {
+            title: "Role Play",
+            startPageNum: 13,
+            endPageNum: 13,
+          },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Three",
+        subtitle: `<span class="text--primary">My Age</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 16, endPageNum: 17 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 18,
+            endPageNum: 19,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 20,
+            endPageNum: 20,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 20,
+            endPageNum: 20,
+          },
+          {
+            title: "Your Conversation",
+            startPageNum: 21,
+            endPageNum: 21,
+          },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Four",
+        subtitle: `<span class="text--primary">My Family</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 22, endPageNum: 23 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 24,
+            endPageNum: 24,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 25,
+            endPageNum: 25,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 25,
+            endPageNum: 25,
+          },
+          {
+            title: "Your Conversation",
+            startPageNum: 25,
+            endPageNum: 25,
+          },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Five",
+        subtitle: `<span class="text--primary">My Appearance</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 28, endPageNum: 29 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 30,
+            endPageNum: 30,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 31,
+            endPageNum: 31,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 31,
+            endPageNum: 31,
+          },
+          {
+            title: "Your Conversation",
+            startPageNum: 31,
+            endPageNum: 31,
+          },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Six",
+        subtitle: `<span class="text--primary">My House</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 32, endPageNum: 33 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 34,
+            endPageNum: 34,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 35,
+            endPageNum: 35,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 35,
+            endPageNum: 35,
+          },
+          {
+            title: "Your Conversation",
+            startPageNum: 35,
+            endPageNum: 35,
+          },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Seven",
+        subtitle: `<span class="text--primary">My Address</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 38, endPageNum: 39 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 40,
+            endPageNum: 40,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 41,
+            endPageNum: 41,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 41,
+            endPageNum: 41,
+          },
+          {
+            title: "Your Conversation",
+            startPageNum: 41,
+            endPageNum: 41,
+          },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Eight",
+        subtitle: `<span class="text--primary">My Favorite Food</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 42, endPageNum: 43 },
+          {
+            title: "Sounds And Letters ",
+            startPageNum: 44,
+            endPageNum: 44,
+          },
+
+          {
+            title: "Listening and Reading",
+            startPageNum: 45,
+            endPageNum: 45,
+          },
+          {
+            title: "Speaking and Writing",
+            startPageNum: 45,
+            endPageNum: 45,
+          },
+          {
+            title: "Your Conversation",
+            startPageNum: 45,
+            endPageNum: 45,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    bookTitle: "انگلیسی هشتم",
+    bookName: "english8",
+    avatar: require("@/assets/image/lesson1.png"),
+    lessons: [
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson one",
+        subtitle: `<span class="text--primary">My Nationality</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 12, endPageNum: 14 },
+          {
+            title: "Spelling and Pronunciation",
+            startPageNum: 15,
+            endPageNum: 15,
+          },
+
+          {
+            title: "Listening and Writing",
+            startPageNum: 16,
+            endPageNum: 16,
+          },
+          {
+            title: "Reading,Speaking and Writing",
+            startPageNum: 17,
+            endPageNum: 17,
+          },
+          { title: "Role play", startPageNum: 17, endPageNum: 17 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Two",
+        subtitle: `<span class="text--primary">My Week</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 18, endPageNum: 20 },
+          {
+            title: "Spelling and Pronunciation",
+            startPageNum: 21,
+            endPageNum: 21,
+          },
+
+          {
+            title: "Listening and Writing",
+            startPageNum: 21,
+            endPageNum: 21,
+          },
+          {
+            title: "Reading,Speaking and Writing",
+            startPageNum: 22,
+            endPageNum: 22,
+          },
+          { title: "Role play", startPageNum: 22, endPageNum: 22 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Three",
+        subtitle: `<span class="text--primary">My Abilities</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 26, endPageNum: 28 },
+          {
+            title: "Spelling and Pronunciation",
+            startPageNum: 29,
+            endPageNum: 29,
+          },
+
+          {
+            title: "Listening and Writing",
+            startPageNum: 30,
+            endPageNum: 30,
+          },
+          {
+            title: "Reading,Speaking and Writing",
+            startPageNum: 31,
+            endPageNum: 31,
+          },
+          { title: "Role play", startPageNum: 32, endPageNum: 32 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Four",
+        subtitle: `<span class="text--primary">My Health</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 34, endPageNum: 36 },
+          {
+            title: "Spelling and Pronunciation",
+            startPageNum: 36,
+            endPageNum: 36,
+          },
+
+          {
+            title: "Listening and Writing",
+            startPageNum: 37,
+            endPageNum: 37,
+          },
+          {
+            title: "Reading,Speaking and Writing",
+            startPageNum: 37,
+            endPageNum: 37,
+          },
+          { title: "Role play", startPageNum: 38, endPageNum: 38 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Five",
+        subtitle: `<span class="text--primary">My City</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 42, endPageNum: 44 },
+          {
+            title: "Spelling and Pronunciation",
+            startPageNum: 45,
+            endPageNum: 45,
+          },
+
+          {
+            title: "Listening and Writing",
+            startPageNum: 46,
+            endPageNum: 46,
+          },
+          {
+            title: "Reading,Speaking and Writing",
+            startPageNum: 46,
+            endPageNum: 46,
+          },
+          { title: "Role play", startPageNum: 47, endPageNum: 47 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Six",
+        subtitle: `<span class="text--primary">My Village</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 48, endPageNum: 50 },
+          {
+            title: "Spelling and Pronunciation",
+            startPageNum: 51,
+            endPageNum: 51,
+          },
+
+          {
+            title: "Listening and Writing",
+            startPageNum: 52,
+            endPageNum: 52,
+          },
+          {
+            title: "Reading,Speaking and Writing",
+            startPageNum: 52,
+            endPageNum: 53,
+          },
+          { title: "Role play", startPageNum: 53, endPageNum: 53 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Seven",
+        subtitle: `<span class="text--primary">My Hobbies</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 54, endPageNum: 56 },
+          {
+            title: "Spelling and Pronunciation",
+            startPageNum: 57,
+            endPageNum: 57,
+          },
+
+          {
+            title: "Listening and Writing",
+            startPageNum: 57,
+            endPageNum: 57,
+          },
+          {
+            title: "Reading,Speaking and Writing",
+            startPageNum: 58,
+            endPageNum: 58,
+          },
+          { title: "Role play", startPageNum: 59, endPageNum: 59 },
+        ],
+      },
+    ],
+  },
+
+  {
+    bookTitle: "انگلیسی نهم",
+    bookName: "english9",
+    avatar: require("@/assets/image/lesson1.png"),
+    lessons: [
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson one",
+        subtitle: `<span class="text--primary">Personality</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 16, endPageNum: 19 },
+          { title: "Language Melody", startPageNum: 20, endPageNum: 19 },
+
+          { title: "Grammar", startPageNum: 21, endPageNum: 29 },
+          {
+            title: "Listening, Reading and Writing",
+            startPageNum: 24,
+            endPageNum: 31,
+          },
+          {
+            title: "Reading,Speaking,Listening and Writing",
+            startPageNum: 26,
+            endPageNum: 31,
+          },
+          { title: "Role play", startPageNum: 27, endPageNum: 27 },
+        ],
+      },
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Two",
+        subtitle: `<span class="text--primary">Travel</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 30, endPageNum: 33 },
+          { title: "Language Melody", startPageNum: 34, endPageNum: 34 },
+
+          { title: "Grammar", startPageNum: 35, endPageNum: 37 },
+          {
+            title: "Listening, Reading and Writing",
+            startPageNum: 38,
+            endPageNum: 39,
+          },
+          {
+            title: "Reading,Speaking,Listening and Writing",
+            startPageNum: 40,
+            endPageNum: 40,
+          },
+          { title: "Role play", startPageNum: 41, endPageNum: 41 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Three",
+        subtitle: `<span class="text--primary">Festivals and Ceremonies</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 50, endPageNum: 53 },
+          { title: "Language Melody", startPageNum: 54, endPageNum: 54 },
+
+          { title: "Grammar", startPageNum: 55, endPageNum: 57 },
+          {
+            title: "Listening, Reading and Writing",
+            startPageNum: 58,
+            endPageNum: 59,
+          },
+          {
+            title: "Reading,Speaking,Listening and Writing",
+            startPageNum: 60,
+            endPageNum: 60,
+          },
+          { title: "Role play", startPageNum: 61, endPageNum: 41 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Four",
+        subtitle: `<span class="text--primary">Services</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 64, endPageNum: 67 },
+          { title: "Language Melody", startPageNum: 68, endPageNum: 68 },
+
+          { title: "Grammar", startPageNum: 69, endPageNum: 71 },
+          {
+            title: "Listening, Reading and Writing",
+            startPageNum: 72,
+            endPageNum: 73,
+          },
+          {
+            title: "Reading,Speaking,Listening and Writing",
+            startPageNum: 74,
+            endPageNum: 74,
+          },
+          { title: "Role play", startPageNum: 75, endPageNum: 75 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Five",
+        subtitle: `<span class="text--primary">Media</span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 82, endPageNum: 85 },
+          { title: "Language Melody", startPageNum: 86, endPageNum: 86 },
+
+          { title: "Grammar", startPageNum: 87, endPageNum: 89 },
+          {
+            title: "Listening, Reading and Writing",
+            startPageNum: 90,
+            endPageNum: 91,
+          },
+          {
+            title: "Reading,Speaking,Listening and Writing",
+            startPageNum: 92,
+            endPageNum: 92,
+          },
+          { title: "Role play", startPageNum: 93, endPageNum: 41 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/lesson1.png"),
+        title: "Lesson Six",
+        subtitle: `<span class="text--primary">Health And Injuries </span> `,
+        sectionItems: [
+          { title: "Conversation", startPageNum: 96, endPageNum: 99 },
+          { title: "Language Melody", startPageNum: 100, endPageNum: 100 },
+
+          { title: "Grammar", startPageNum: 101, endPageNum: 103 },
+          {
+            title: "Listening, Reading and Writing",
+            startPageNum: 104,
+            endPageNum: 105,
+          },
+          {
+            title: "Reading,Speaking,Listening and Writing",
+            startPageNum: 106,
+            endPageNum: 106,
+          },
+          { title: "Role play", startPageNum: 107, endPageNum: 107 },
+        ],
+      },
+
+      {
+        avatar: require("@/assets/image/appendix.png"),
+        title: "Appendix",
+        sectionItems: [
+          { title: "Irregular Verbs", startPageNum: 131, endPageNum: 133 },
+        ],
+        subtitle: `<span class="text--primary">Irregular Verbs</span> `,
+      },
+    ],
+  },
+
+  {
     bookTitle: "انگلیسی دهم",
     bookName: "english10",
     pageOffset: 2,
